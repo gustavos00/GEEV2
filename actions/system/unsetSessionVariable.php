4497a9fa1122d8c7315199adb47465560c9b0c9e
Gustavo Santos <z.gsantos20@gmail.com>
2021-07-06 16:19:31 +0000

    adding a .gitignore

--- actions/system/unsetSessionVariable.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$data = json_decode(file_get_contents("php://input"));
-$variable = $data->variable;
-
-if(isset($_SESSION[$variable])) {
-    unset($_SESSION[$variable]);
-}
